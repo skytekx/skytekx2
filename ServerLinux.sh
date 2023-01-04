0f8c001227cc2fd44f7dd8a77f6a48c28a37a059
root <root@craftwork.local>
2023-01-04 12:29:11 +0000

    WIP: add tekxit mods

---
+++ ServerLinux.sh
@@ -0,0 +1,1 @@
+java -server -Xmx4G -Xms4G -jar forge-1.12.2-14.23.5.2860.jar nogui
---
+++ ServerWindows.bat
@@ -0,0 +1,2 @@
+java -server -Xmx4G -Xms4G -Dlog4j.configurationFile=log4jformattingfix.xml -jar forge-1.12.2-14.23.5.2860.jar nogui
+Pause >nul
