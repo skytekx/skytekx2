66cb0273a595c2ab549cd230db661ebd49ad7abb
Florian Kleber <kleber@snek.at>
2023-01-05 15:38:14 +0100

    WIP: add TekxitPiServer

--- start.bat
+++ start.bat
@@ -3,11 +3,11 @@
 :: When setting the memory below make sure to include the amount of ram letter. M = MB, G = GB. Don't use 5GB for example, it's 5G ::
 
 :: This is allocated memory ::
-set mem=3G
+set mem=8G
 
 :: Don't edit past this point ::
 ECHO [Hexxit II] Server is starting...
 
-java -server -Xmx%mem% -XX:+UseG1GC -Xms2G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dfml.queryResult=confirm -Duser.language=en -jar forge-1.12.2-14.23.5.2860.jar nogui
+java -server -Xmx%mem% -XX:+UseG1GC -Xms2G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -Dfml.queryResult=confirm -Duser.language=en -jar mohist.jar nogui
 
 PAUSE
